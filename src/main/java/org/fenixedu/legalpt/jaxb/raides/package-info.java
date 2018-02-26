4361693e2c0db1d6d8cd3fd6b5f3a460440f37a1
Nadir Amin <nadir.amin@qub-it.com>
2018-02-26 10:48:58 +0000

    Improved xml namespace import

---
+++ src/main/java/org/fenixedu/legalpt/jaxb/raides/package-info.java
@@ -0,0 +1,5 @@
+@XmlSchema(xmlns = { @XmlNs(prefix = "xsi", namespaceURI = "http://www.w3.org/2001/XMLSchema-instance") })
+package org.fenixedu.legalpt.jaxb.raides;
+
+import javax.xml.bind.annotation.XmlNs;
+import javax.xml.bind.annotation.XmlSchema;
