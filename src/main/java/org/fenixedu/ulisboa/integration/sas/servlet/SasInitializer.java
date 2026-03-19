a6e88a28b646c4765401d83b0a9563e02db527d6
eduardofernandes-qubit <eduardo.fernandes@qub-it.com>
2026-03-19 13:59:42 +0000

    Delete IDDocumentType legal mapping (#38)
    Relates with: #qubIT-Fenix-6491

--- src/main/java/org/fenixedu/ulisboa/integration/sas/servlet/SasInitializer.java
+++ src/main/java/org/fenixedu/ulisboa/integration/sas/servlet/SasInitializer.java
@@ -5,9 +5,12 @@ import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
 import org.fenixedu.bennu.core.domain.Bennu;
+import org.fenixedu.legalpt.domain.mapping.EnumerationLegalMapping;
 import org.fenixedu.ulisboa.integration.sas.domain.EducationLevelTypeMapping;
 import org.fenixedu.ulisboa.integration.sas.domain.SocialServicesConfiguration;
 import org.fenixedu.ulisboa.integration.sas.service.sicabe.SicabeExternalService;
+
+import com.qubit.terra.framework.services.logging.Log;
 
 import pt.ist.fenixframework.Atomic;
 import pt.ist.fenixframework.Atomic.TxMode;
@@ -26,9 +29,29 @@ public class SasInitializer implements ServletContextListener {
         EducationLevelTypeMapping.registerEvents();
 
         SicabeExternalService.init();
+        deleteIDDocumentTypeEnumerationMappings();
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent event) {
+    }
+
+    @Atomic(mode = TxMode.WRITE)
+    private void deleteIDDocumentTypeEnumerationMappings() {
+        String sourceType = "ID_DOCUMENT_TYPE";
+
+        EnumerationLegalMapping sourceMapping =
+                Bennu.getInstance().getLegalMappingsSet().stream().filter(EnumerationLegalMapping.class::isInstance)
+                        .map(EnumerationLegalMapping.class::cast).filter(mapping -> mapping.getType().equals(sourceType))
+                        .findFirst().orElse(null);
+
+        if (sourceMapping == null) {
+            Log.warn("Source EnumerationLegalMapping not found for type: " + sourceType);
+            return;
+        }
+
+        sourceMapping.getLegalMappingEntriesSet().forEach(sourceMapping::deleteEntry);
+        sourceMapping.delete();
+        Log.warn("Source ID_DOCUMENT_TYPE EnumerationLegalMapping entries and mapping deleted");
     }
 }
